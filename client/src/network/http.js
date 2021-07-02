f8f570dce791da2e9b3f10c95ec0a8c6e07b18a9
Jisoo <helloworld.jisoo@gmail.com>
2021-07-02 09:40:12 +0100

    improve frontside

---
+++ client/src/network/http.js
@@ -0,0 +1,26 @@
+export default class HttpClient{
+  constructor(baseURL){
+    this.baseURL = baseURL;
+  }
+
+  async fetch(url,options){
+    const response = await fetch(`${this.baseURL}${url}`,{
+      ...options,
+      headers: {
+        'Content-Type': 'application/json',
+        ...options.header
+      }
+    });
+    let data;
+    try {
+      data = await response.json();
+    } catch (error) {
+      console.error(error);
+    }
+    if (response.status >299 || response.status < 200){
+      const message = data && data.message ? data.message : 'Something went wrong 👻'
+      throw new Error(message);
+    }
+    return data;
+  }
+}
--- client/src/service/tweet.js
+++ client/src/service/tweet.js
@@ -1,59 +1,33 @@
 export default class TweetService {
   
-  constructor(baseURL){
-    this.baseURL = baseURL;
+  constructor(http){
+    this.http = http;
   }
 
   async getTweets(username) {
     const query = username ? `?username = ${username}`:'';
-    const response = await fetch(`${this.baseURL}/tweets${query}`,{
+    return this.http.fetch/(`tweets${query}`,{
       method:'GET',
-      header:{ 'Content-type': 'application.json'},
     });
-    const data = await response.json();
-    if(response.status!==200){
-      throw new Error(data.message);
-    }
-
-    return data;
-    
   }
 
   async postTweet(text) {
-    const response = await fetch(`${this.baseURL}/tweets`,{
+    return this.http.fetch(` /tweets`,{
       method:'POST',
-      header:{ 'Content-type': 'application.json'},
       body: JSON.stringify({text,username:'jisoo',name:'Jisoo'})
     });
-    const data = await response.json();
-    if(response.status!==201){
-      throw new Error(data.message);
-    }
-    return data;
-    
   }
 
   async deleteTweet(tweetId) {
-   const response = await fetch(`${this.baseURL}/tweets/${tweetId}`,{
+   return this.http.fetch(` /tweets/${tweetId}`,{
       method:'DELETE',
-      header:{ 'Content-type': 'application.json'},
     });
-    if(response.status!==204){
-      throw new Error();
-    }
   }
 
   async updateTweet(tweetId, text) {
-const response = await fetch(`${this.baseURL}/tweets/${tweetId}`,{
+return this.http.fetch(` /tweets/${tweetId}`,{
       method:'PUT',
-      header:{ 'Content-type': 'application.json'},
-          body: JSON.stringify({text})
+      body: JSON.stringify({text})
     });
- const data = await response.json();
-    if(response.status!==200){
-      throw new Error(data.message);
-    }
-
-    return data;
-  }
+}
 }
