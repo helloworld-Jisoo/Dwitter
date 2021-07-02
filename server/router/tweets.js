007eb56b0da9656044deeeb9ed60e4cd6033e0a6
Jisoo <helloworld.jisoo@gmail.com>
2021-07-02 15:09:35 +0100

    validate update

--- server/router/tweets.js
+++ server/router/tweets.js
@@ -1,16 +1,25 @@
 import express from 'express';
 import 'express-async-errors';
+import { body } from 'express-validator';
 import * as tweetController from '../controller/tweet.js';
+import { validate } from '../middleware/validator.js';
 
 const router = express.Router();
+const validatteTweet = [
+  body('text')
+  .trim()
+  .isLength({min:3})
+  .withMessage('text should be at least 3 characters'),
+  validate
+]
 
 router.get('/', tweetController.getTweets);
 
 router.get('/:id', tweetController.getTweet);
 
-router.post('/', tweetController.createTweet);
+router.post('/', validatteTweet,tweetController.createTweet);
 
-router.put('/:id', tweetController.updateTweet);
+router.put('/:id', validatteTweet, tweetController.updateTweet);
 
 router.delete('/:id',tweetController.deleteTweet);
 
