f90008f2f33858160e51075e41f0eb2d49acf1b0
Jisoo <helloworld.jisoo@gmail.com>
2021-07-12 17:44:08 +0100

    update tweet.js

--- server/db/database.js
+++ server/db/database.js
@@ -1,19 +1,11 @@
-import mysql from 'mysql2';
+
 import { config } from '../config.js';
 import SQ from 'sequelize';
 
 const {host, user, database, password} = config.db;
 export const sequelize = new SQ.Sequelize(database, user, password, {
   host,
-  dialect : 'mysql', // Default
+  dialect :'mysql', // Default
+  logging : false,
 })
 
-
-const pool = mysql.createPool({
-  host,
-  user,
-  database,
-  password,
-})
-
-export const db = pool.promise();
