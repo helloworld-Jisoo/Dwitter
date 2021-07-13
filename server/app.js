b335b44bd47d229ed45ed8315af7079fb832db12
Jisoo <helloworld.jisoo@gmail.com>
2021-07-13 10:35:03 +0100

    mongoose connect

--- server/app.js
+++ server/app.js
@@ -30,6 +30,7 @@ app.use((req,res,next)=>{
 
   connectDB()
   .then(() => {
+    console.log("init!");
     const server = app.listen(config.host.port);
     initSocket(server);
   })
--- server/db/database.js
+++ server/db/database.js
@@ -1,20 +1,18 @@
-
-import MongoDb from 'mongodb';
+import Mongoose from 'mongoose';
 import { config } from '../config.js';
 
-let db;
 
 export function connectDB(){
-  return MongoDb.MongoClient.connect(config.db.host,{
+  return Mongoose.connect(config.db.host,{
     useNewUrlParser: true,
     useUnifiedTopology: true,
-  }).then ((client) => {
-    db = client.db();
+    useFindAndModify: false,
   })
 }
 
-// collections 
+//Todo(Jisoo): Delete below
 
+let db;
 export function getUsers() {
   return db.collection('users');
 }
