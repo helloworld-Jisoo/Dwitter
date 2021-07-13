6568cd77fe630c342604d10f729cb655bb5c207b
Jisoo <helloworld.jisoo@gmail.com>
2021-07-13 09:22:55 +0100

    mongodb install and connect

--- server/app.js
+++ server/app.js
@@ -7,7 +7,7 @@ import tweetsRouter from './router/tweets.js';
 import authRouter from "./router/auth.js";
 import { config } from './config.js';
 import { initSocket } from './connection/socket.js'
-import { db } from './db/database.js';
+import { connectDB } from './db/database.js';
 
 const app = express();
 
@@ -27,6 +27,10 @@ app.use((req,res,next)=>{
   console.error(error);
   res.sendStatus(500);
 })
-// db.getConnection().then((connection) => console.log(connection));
-const server = app.listen(config.host.port);
-initSocket(server);
+
+  connectDB()
+  .then(() => {
+    const server = app.listen(config.host.port);
+    initSocket(server);
+  })
+  .catch(console.error);
